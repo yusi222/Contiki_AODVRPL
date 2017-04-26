7beab147d1f4f7022b6bf21268ac819c88fdf60c
root <lavanyahm@ece.iisc.ernet.in>
2017-04-26 14:51:44 +0530

    Duplicate remove

--- Scripts/command_MultiNode.py
+++ Scripts/command_MultiNode.py
@@ -17,7 +17,12 @@ filename      = args.infile
 dest_filename = args.infile + file_ex
  
 print "log file :  %s "%filename
- 
+
+PACKET_SIZE_BYTE = 40 
+BIT_PER_BYTE     =8 
+MILLSECOND_TO_SECOND= 0.001
+
+
 No_data_pkt_sent = 0
 No_data_pkt_received = 0
 pkt_sent_list = []
@@ -30,13 +35,14 @@ delay_list =[]
 last_rec =0
 
 senter_1 = "ID:10"
-reciever_1 = "ID:7"
+reciever_1 = "ID:8"
 
 senter_list_1 = []
 reciever_list_1 = []
 delay_list_1 =[]
+throughput_list =[]
 final_delay_list =[] 
-
+Avg_throughput = 0
 print"-----------------------------------------"
 print"Extracting Data from File :%s "%filename
 print"-----------------------------------------"
@@ -52,7 +58,7 @@ for line in f:
         if sentList[3] not in pkt_sent_list:
 	     pkt_sent_list.insert(0,sentList[3])  
              No_data_pkt_sent +=1
-        senter_list_1.append(subList) 
+             senter_list_1.append(subList) 
     elif  (re.findall (reciever_1,line) and re.findall("Server_receivedHello.*",line)):  
         reciveList = line.split(None, 6) 
         subList =[]
@@ -61,7 +67,7 @@ for line in f:
         if reciveList[3] not in pkt_received_list :
       		pkt_received_list.insert(0,reciveList[3]) 
         	No_data_pkt_received +=1
-        reciever_list_1.append(subList)
+                reciever_list_1.append(subList)
 
     if (re.findall("RPL: Sending a DIS.*",line)  ):     
         No_DIS_pkt +=1
@@ -82,18 +88,21 @@ for send_pkt in senter_list_1:
     for recivepkt in reciever_list_1:
          if send_pkt[0] == recivepkt[0]:
 	    delay = int(recivepkt[1]) - int(send_pkt[1])
+            throughput = float((PACKET_SIZE_BYTE * BIT_PER_BYTE))/  float(delay* MILLSECOND_TO_SECOND)
             delay_list = []
             delay_list.insert(0,send_pkt[0])
             delay_list.insert(1,send_pkt[1])
             delay_list.insert(2,recivepkt[1]) 
             delay_list.insert(3, delay)
+            delay_list.insert(4,throughput )
 	    delay_list_1.append(delay_list)
+            throughput_list.insert(0,throughput)
             final_delay_list.insert(0,delay)
-
 
 last_sent = sentList[3]
 PDR = float (len(pkt_received_list)) / float(len(pkt_sent_list))
 Avg_delay = sum(final_delay_list) /float(len(final_delay_list))
+Avg_throughput = sum(throughput_list) /float(len(throughput_list))
 Control_packet = No_DIS_pkt + No_DIO_pkt + No_DIO_pkt + No_DAO_pkt
 
 print"-----------------------------------------"
@@ -104,7 +113,8 @@ print "No_data_pkt_received :%d "%len(pkt_received_list)
 print "No of Pkt Droped %d "% ( No_data_pkt_sent- No_data_pkt_received)
 print "No of Control Packets %d "%Control_packet
 print "PDR :%f"%PDR 
-print "AVg_Delay : %d milliseconds"%Avg_delay
+print "AVg_Delay : %f milliseconds"%Avg_delay
+print "Avg_throughput : %d  bits per second"%Avg_throughput
 
 print"-----------------------------------------"
 print"       Writing Data into %s" %dest_filename
@@ -127,6 +137,14 @@ thefile.write("\n")
 thefile.write("PDR,")
 thefile.write("%s," % PDR)
 
+thefile.write("\n")
+thefile.write("Avg_delay,")
+thefile.write("%s milliseconds " % Avg_delay)
+thefile.write("\n")
+
+thefile.write("Avg_throughput,")
+thefile.write("%s bitssecond " % Avg_throughput)
+thefile.write("\n")
 
 thefile.write("\n")
 thefile.write("No_DIS_pkt,")
@@ -146,21 +164,16 @@ thefile.write("%s," % Control_packet)
 thefile.write("\n")
 
 
-thefile.write("Avg_delay,")
-thefile.write("%s milliseconds " % Avg_delay)
-thefile.write("\n")
-
-
-thefile.write("\n -------------------------------\n") 
+thefile.write("\n ----,-----------------------,--------\n") 
 thefile.write("\n DETAILS:------\n") 
-thefile.write("\n ---------------------------------\n") 
+thefile.write("\n -----,----------------------,------\n") 
 thefile.write("Source,")
 thefile.write("Destination,")
 thefile.write("Packet_No,")
 thefile.write("Sent_Time,")
 thefile.write("Received_time,")
 thefile.write("Delay(milliseconds) ,\n")
-
+thefile.write("Throughput (bits_per_second)\n")
 thefile.write(senter_1)
 thefile.write(",")
 thefile.write(reciever_1)
@@ -171,6 +184,8 @@ for item in  delay_list_1:
       thefile.write("%s," % conte)
     thefile.write("\n")
     thefile.write(",,")
+
+
 thefile.close()
 print"-----------------------------------------"
 print"           END"
